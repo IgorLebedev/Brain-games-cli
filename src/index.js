48abf7fc71e44747fc6940cfd291dc7746082997
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-07 13:39:10 +0700

    game engine rework

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import readlineSync from 'readline-sync';
 
-export const sayHelloAndGetName = () => {
+const startGame = (rules, taskAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-export const getRandomNum = (min, max) => {
-  const result = Math.floor(min + Math.random() * (max + 1 - min));
-  return result;
-};
-
-export const victoryCondition = (rounds, userName) => {
-  if (rounds === 3) {
-    console.log(`Congratulations, ${userName}!`);
+  console.log(rules);
+  const maxRounds = 3;
+  for (let i = 0; i < maxRounds; i += 1) {
+    const repeat = taskAnswer();
+    const question = repeat[0];
+    const correctAnswer = repeat[1];
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your Answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
   }
+  console.log(`Congratulations, ${userName}!`);
 };
+
+export default startGame;
