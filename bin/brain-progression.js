ad045311f8dbd38c858e988418722faf451e3379
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-07 14:39:51 +0700

    rework progression game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../games/progression.js';
+import startProgression from '../src/games/progression.js';
 
-startGame();
+startProgression();
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,27 @@
-import readlineSync from 'readline-sync';
-import { sayHelloAndGetName, getRandomNum, victoryCondition } from '../index.js';
+import getRandomNum from '../utils.js';
+import startGame from '../index.js';
 
-const startGame = () => {
-  const userName = sayHelloAndGetName();
-  console.log('What number is missing in the progression?');
-  let rounds = 0;
-  do {
-    const randomFirstNum = getRandomNum(1, 100);
-    const randomStep = getRandomNum(1, 5);
-    const randomLength = getRandomNum(5, 10);
-    const randomEmpty = getRandomNum(0, randomLength - 1);
-    const numArr = [];
-    for (let i = 0; i < randomLength; i += 1) {
-      if (numArr.length === 0) {
-        numArr.push(randomFirstNum);
-      } else numArr.push(numArr[i - 1] + randomStep);
-    }
-    const result = numArr[randomEmpty];
-    numArr[randomEmpty] = '..';
-    console.log(`Question: ${numArr.join(' ')}`);
-    const answer = readlineSync.question('Your Answer: ');
-    const isCorrectAnswer = result === Number(answer);
-    if (isCorrectAnswer) {
-      console.log('Correct!');
-      rounds += 1;
-    } else if (!isCorrectAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`);
-      break;
-    }
-  } while (rounds < 3);
-  victoryCondition(rounds, userName);
+const rules = 'What number is missing in the progression?';
+
+const getTaskAndAnswer = () => {
+  const taskAnswer = [];
+  const randomFirstNum = getRandomNum(1, 100);
+  const randomStep = getRandomNum(1, 5);
+  const randomLength = getRandomNum(5, 10);
+  const randomEmpty = getRandomNum(0, randomLength - 1);
+  const numArr = [];
+  for (let i = 0; i < randomLength; i += 1) {
+    if (numArr.length === 0) {
+      numArr.push(randomFirstNum);
+    } else numArr.push(numArr[i - 1] + randomStep);
+  }
+  taskAnswer[1] = (numArr[randomEmpty]).toString();
+  numArr[randomEmpty] = '..';
+  const result = numArr.join(' ');
+  taskAnswer[0] = result;
+  return taskAnswer;
 };
 
-export default startGame;
+const startProgression = () => startGame(rules, getTaskAndAnswer);
+
+export default startProgression;
