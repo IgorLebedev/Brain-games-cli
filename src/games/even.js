33f2d7f3f68b6bb47c0089dfddee146be47406ee
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-09 15:22:45 +0700

    moved isEven to a separate function, rework

--- src/games/even.js
+++ src/games/even.js
@@ -3,16 +3,13 @@ import startGame from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
 const getTaskAndAnswer = () => {
-  const taskAnswerArr = [];
   const randomNum = getRandomNum(1, 100);
-  taskAnswerArr.push(randomNum);
-  if (randomNum % 2 === 0) {
-    taskAnswerArr.push('yes');
-  } else {
-    taskAnswerArr.push('no');
-  }
-  return taskAnswerArr;
+  const question = randomNum;
+  const correctAnswer = isEven(randomNum);
+  return [question, correctAnswer];
 };
 
 const startEven = () => startGame(rules, getTaskAndAnswer);
