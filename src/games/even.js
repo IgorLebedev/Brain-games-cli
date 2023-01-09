d20ff70cdf42ddcfbb51d9ec82474fd975ccc011
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-09 16:21:06 +0700

    rework isEven to return boolean

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,12 @@ import startGame from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 
 const getTaskAndAnswer = () => {
   const randomNum = getRandomNum(1, 100);
   const question = randomNum;
-  const correctAnswer = isEven(randomNum);
+  const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
