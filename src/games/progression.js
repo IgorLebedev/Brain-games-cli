d2680ef8d32587a6df0af98a752273395738c8a0
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-09 15:26:01 +0700

    moved getRandomNum to a separate function, rework

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,23 +3,26 @@ import startGame from '../index.js';
 
 const rules = 'What number is missing in the progression?';
 
+const getRandomNumArr = (firstNum, step, arrLength) => {
+  const numArr = [];
+  numArr.push(firstNum);
+  for (let i = 1; i < arrLength; i += 1) {
+    const previousNum = numArr[i - 1];
+    numArr.push(previousNum + step);
+  }
+  return numArr;
+};
+
 const getTaskAndAnswer = () => {
-  const taskAnswer = [];
   const randomFirstNum = getRandomNum(1, 100);
   const randomStep = getRandomNum(1, 5);
   const randomLength = getRandomNum(5, 10);
   const randomEmpty = getRandomNum(0, randomLength - 1);
-  const numArr = [];
-  for (let i = 0; i < randomLength; i += 1) {
-    if (numArr.length === 0) {
-      numArr.push(randomFirstNum);
-    } else numArr.push(numArr[i - 1] + randomStep);
-  }
-  taskAnswer[1] = (numArr[randomEmpty]).toString();
+  const numArr = getRandomNumArr(randomFirstNum, randomStep, randomLength);
+  const correctAnswer = (numArr[randomEmpty]).toString();
   numArr[randomEmpty] = '..';
-  const result = numArr.join(' ');
-  taskAnswer[0] = result;
-  return taskAnswer;
+  const question = numArr.join(' ');
+  return [question, correctAnswer];
 };
 
 const startProgression = () => startGame(rules, getTaskAndAnswer);
