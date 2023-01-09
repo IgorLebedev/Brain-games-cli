b39ec217ee529021777e5858a857ea70d3d634c6
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-09 14:45:50 +0700

    minor fix

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,30 +3,27 @@ import startGame from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
-const getResult = (operator, randomNumOne, randomNumTwo) => {
+const getResult = (operator, firstNum, SecondNum) => {
   switch (operator) {
     case '+':
-      return ((randomNumOne + randomNumTwo).toString());
+      return ((firstNum + SecondNum).toString());
     case '-':
-      return ((randomNumOne - randomNumTwo).toString());
+      return ((firstNum - SecondNum).toString());
     case '*':
-      return ((randomNumOne * randomNumTwo).toString());
+      return ((firstNum * SecondNum).toString());
     default:
       throw new Error(`Unknown operator: '${operator}'!`);
   }
 };
 
 const getTaskAndAnswer = () => {
-  let question = '';
-  let correctAnswer = '';
   const operators = ['+', '-', '*'];
-  const lastOperatorIdx = operators.length - 1;
-  const randomIndex = getRandomNum(0, lastOperatorIdx);
+  const randomIndex = getRandomNum(0, operators.length - 1);
   const randomNumOne = getRandomNum(1, 10);
   const randomNumTwo = getRandomNum(1, 10);
   const operator = operators[randomIndex];
-  question = `${randomNumOne} ${operator} ${randomNumTwo}`;
-  correctAnswer = getResult(operator, randomNumOne, randomNumTwo);
+  const question = `${randomNumOne} ${operator} ${randomNumTwo}`;
+  const correctAnswer = getResult(operator, randomNumOne, randomNumTwo);
   return [question, correctAnswer];
 };
 
