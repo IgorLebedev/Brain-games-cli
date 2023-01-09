db32fef307113dd5e97e8ba8898d9653085b4541
igorlebedev <lebedew.igor400@yandex.ru>
2023-01-09 14:10:22 +0700

    moved getResult to a separate function, little rework

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,28 +3,31 @@ import startGame from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
-const getTaskAndAnswer = () => {
-  const taskAnswer = [];
-  const operators = ['+', '-', '*'];
-  const randomIndex = getRandomNum(0, 2);
-  const randomNumOne = getRandomNum(1, 10);
-  const randomNumTwo = getRandomNum(1, 10);
-  const operator = operators[randomIndex];
-  taskAnswer.push(`${randomNumOne} ${operator} ${randomNumTwo}`);
+const getResult = (operator, randomNumOne, randomNumTwo) => {
   switch (operator) {
     case '+':
-      taskAnswer.push((randomNumOne + randomNumTwo).toString());
-      break;
+      return ((randomNumOne + randomNumTwo).toString());
     case '-':
-      taskAnswer.push((randomNumOne - randomNumTwo).toString());
-      break;
+      return ((randomNumOne - randomNumTwo).toString());
     case '*':
-      taskAnswer.push((randomNumOne * randomNumTwo).toString());
-      break;
+      return ((randomNumOne * randomNumTwo).toString());
     default:
       throw new Error(`Unknown operator: '${operator}'!`);
   }
-  return taskAnswer;
+};
+
+const getTaskAndAnswer = () => {
+  let question = '';
+  let correctAnswer = '';
+  const operators = ['+', '-', '*'];
+  const lastOperatorIdx = operators.length - 1;
+  const randomIndex = getRandomNum(0, lastOperatorIdx);
+  const randomNumOne = getRandomNum(1, 10);
+  const randomNumTwo = getRandomNum(1, 10);
+  const operator = operators[randomIndex];
+  question = `${randomNumOne} ${operator} ${randomNumTwo}`;
+  correctAnswer = getResult(operator, randomNumOne, randomNumTwo);
+  return [question, correctAnswer];
 };
 
 const startCalc = () => startGame(rules, getTaskAndAnswer);
